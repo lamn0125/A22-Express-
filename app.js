b93103421a7116874f19d3a48a6b6c72c7b9b055
lamn0125 <lamn0125@gmail.com>
2023-03-30 13:56:58 +0800

    feat: Add routes

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('this is my express project')
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
 })
 
 app.listen(port, () => {
