5d459dff802fee9cd141f65584510d5541500848
lamn0125 <lamn0125@gmail.com>
2023-04-02 14:31:56 +0800

    feat: Capitalize title first letter

--- app.js
+++ app.js
@@ -15,9 +15,22 @@ app.get('/', (req, res) => {
 
 app.get('/:page', (req, res) => {
     const page = req.params.page
-    res.render('website', {page})
+    const uppercasePageTitle =  page.charAt(0).toUpperCase() + page.slice(1);
+
+    return res.render('website', { page: uppercasePageTitle })
 })
 
 app.listen(port, () => {
   console.log(`Express running on localhost:${port}`)
 })
+
+
+//app.get('/:page', (req, res) => {
+//     const page = req.params.page
+//     const uppercasePageTitle = 這裡實作page首字大寫的轉換
+//     return res.render('website', { page: uppercasePageTitle }) // 把uppercasePageTitle作為page變數傳入
+// })
+
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
